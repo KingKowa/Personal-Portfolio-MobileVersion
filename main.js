7d31d2a39b2f3497f23ac998a81e5ad61e03ce7d
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-10 15:06:01 +0000

    Modify main.js file

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 const menuIcon = document.querySelector(".menu-icon");
-const mobileNav = document.querySelector(".mobile-version-nav");
-const mobileMenuItems = document.querySelectorAll(".mobile-version-nav .mobile-menu-items li");
-
-
-menuIcon.addEventListener("click",() => {mobileNav.classList.add("active");});
