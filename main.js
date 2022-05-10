9793a571eb116a0f7ade9b98696c67eecbf8ac9a
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-10 22:01:30 +0000

    Fix errors with main.js file

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const mobileNav = document.querySelector('.mobile-version-nav');
 const menuIcon = document.querySelector('.menu-icon');
-//const mobileMenuItems = document.querySelector(".mobile-version-nav");
 const closeButton = document.querySelector('.close-button');
 
 menuIcon.addEventListener('click', () => {
-     mobileNav.classList.add('active');
+  mobileNav.classList.add('active');
 });
 
-closeButton.addEventListener('click', () => { 
-    mobileNav.classList.remove('active');
+closeButton.addEventListener('click', () => {
+  mobileNav.classList.remove('active');
 });
