1906acc6b085b1667f7e211763e4de9ef6616827
robinbenitezmora <robinbenitez1990@gmail.com>
2022-05-10 10:22:38 -0500

    Add code in the main.js file in order to create the general menu

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const menuIcon = document.querySelector(".menu-icon");
+let mobileNav = document.querySelector(".mobile-version-nav");
+let mobileMenuItems = document.querySelectorAll(".mobile-version-nav .mobile-menu-items li");
+menuIcon.addEventListener("click", () => {
+  mobileNav.classList.add("active");
+});
+
