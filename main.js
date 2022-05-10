f3b2b1a42c3c7471048ed7e1bfa44ef485a85def
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-10 20:39:07 +0000

    Add code to complete the project

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-let mobileNav = document.querySelector(".mobile-version-nav");
-let mobileMenuItems = document.querySelectorAll(".mobile-version-nav .mobile-menu-items li");
-menuIcon.addEventListener("click", () => {
-  mobileNav.classList.add("active");
+const mobileNav = document.querySelector(".mobile-version-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const mobileMenuItems = document.querySelector(".mobile-version-nav");
+const closeButton = document.querySelector(".close-button");
+
+menuIcon.addEventListener("click", () => { mobileNav.classList.add("active");
 });
 
+closeButton.addEventListener("click", () => { mobileNav.classList.remove("active");
+});
