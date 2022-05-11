19355eb806e23f98f365eb8734097739c5493e27
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-11 11:03:02 +0000

    Modify sections of code to address issues in the code review

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const mobileNav = document.querySelector('.mobile-version-nav');
 const menuIcon = document.querySelector('.menu-icon');
 const closeButton = document.querySelector('.close-button');
+const menuPort = document.querySelector('.portfolio');
+const menuAbt = document.querySelector('.about');
+const menuCon = document.querySelector('.contact');
+
 
 menuIcon.addEventListener('click', () => {
   mobileNav.classList.add('active');
@@ -9,3 +13,17 @@ menuIcon.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   mobileNav.classList.remove('active');
 });
+
+menuPort.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+  });
+
+menuAbt.addEventListener('click', () => {
+  mobileNav.classList.remove('active');
+});
+
+menuCon.addEventListener('click', () => {
+  mobileNav.classList.remove('active');
+});
+
+
