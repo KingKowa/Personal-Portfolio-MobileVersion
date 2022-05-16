0e68013306dff966cf48a6c70e8734364915977f
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-16 20:24:40 +0000

    Create variables in main.js file

---
+++ main.js
@@ -0,0 +1,10 @@
+const mobileNav = document.querySelector('.mobile-version-nav');
+const menuIcon = document.querySelector('.menu-icon');
+const closeButton = document.querySelector('.close-button');
+const menuPort = document.querySelector('.portfolio');
+const menuAbt = document.querySelector('.about');
+const menuCon = document.querySelector('.contact');
+
+menuIcon.addEventListener('click', () => {
+  mobileNav.classList.add('active');
+});
